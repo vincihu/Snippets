60f811ea84d17ecaf025be83615af3d4a4c01098
vincihu <vincihu@tencent.com>
2020-03-22 12:53:34 +0800

    Termius

---
+++ QuantumultX/Scripts/Termius.js
@@ -0,0 +1,9 @@
+//https:\/\/api\.termius\.com\/api\/v3\/bulk\/account\/ url script-response-body Terminus.js
+//api.termius.com
+let obj=JSON.parse($response.body)
+obj.account["pro_mode"] = true;
+obj.account["plan_type"] = "Premium";
+obj.account["user_type"] = "Premium";
+obj.account["current_period"]["until"] = "2021-01-01T00:00:00";
+$done({body:JSON.stringify(obj)})
+//bynubyta
